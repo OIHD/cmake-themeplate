4b96d6c4a477930ef26fed0890b3e45de5582647
Göktuğ <oihdofficial@outlook.com>
2023-03-06 15:02:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GITHEMEF="$HOME/cmake-themeplate/"
+cd $GITHEMEF/ && rm -rf .git
 cd $GITHEMEF/themeplate && rm -rf build
 cd $GITHEMEF && rm -rf buildthemeplate
 cd $GITHEMEF && mkdir buildthemeplate
