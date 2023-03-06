3c4ea33caaaf98a94be1900e287de12280996355
Göktuğ <oihdofficial@outlook.com>
2023-03-06 15:40:41 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 GITHEMEF="$HOME/cmake-themeplate/"
 GITHEMEFT="$HOME/cmake-themeplate/themeplate"
-cd $GITHEMEF/themeplate && mv vscode/ .vscode
 cd $GITHEMEF/ && rm -rf .git
+if [ -d "$GITHEMEF/themeplate/vscode" ]; then
+    cd $GITHEMEF/themeplate && mv vscode/ .vscode
+fi
 if [ -d "$GITHEMEFT/build" ]; then
     rm -rf "$GITHEMEFT/build"   
 fi
