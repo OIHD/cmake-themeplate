6df7bd73b536d2705f502ac137b00af78789fd19
Göktuğ <oihdofficial@outlook.com>
2023-03-08 13:31:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 GITHEMEF="$HOME/cmake-themeplate/"
 GITHEMEFT="$HOME/cmake-themeplate/themeplate"
+GITHEMEFN="cmake-themeplate"
 cd $GITHEMEF/ && rm -rf .git
+
+echo "Lutfen dosya ismi girin: "
+read DOSYAISMI
+echo "cmake-themeplate klasorunun ismi bu sekilde degistirilecek : $DOSYAISMI"
+
 if [ -d "$GITHEMEF/themeplate/vscode" ]; then
     cd $GITHEMEF/themeplate && mv vscode/ .vscode
 fi
@@ -14,5 +20,14 @@ fi
 if [ -d "$GITHEMEFT/extra" ]; then
     rm -rf "$GITHEMEF/extra"   
 fi
+
 cd $GITHEMEF && mkdir buildthemeplate
 cd $GITHEMEF/themeplate && mkdir build
+
+for  in $3
+do
+    sed -i "s/$GITHEMEFN/$DOSYAISMI/g" $GITHEMEF
+done
+
+cd $HOME
+mv cmake-themeplate $DOSYAISMI
